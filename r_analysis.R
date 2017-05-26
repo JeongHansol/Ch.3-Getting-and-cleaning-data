6514d5f59f260072d32ddaac044e81dfca154eb6
JeongHansol <gksthfaos1145@gmail.com>
2017-05-26 17:30:07 +0900

    analysis code

---
+++ r_analysis.R
@@ -0,0 +1,50 @@
+# reading train set
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+
+# reading test set
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+
+features <- read.table('UCI HAR Dataset/features.txt')
+activityLabels = read.table('UCI HAR Dataset/activity_labels.txt')
+
+# Assining column names(train)
+colnames(x_train) <- features[,2] 
+colnames(y_train) <-"activityId"
+colnames(subject_train) <- "subjectId"
+# Assining column names(test)
+colnames(x_test) <- features[,2] 
+colnames(y_test) <- "activityId"
+colnames(subject_test) <- "subjectId"
+
+colnames(activityLabels) <- c('activityId','activityType')
+# Merging all data in one set
+mrg_train <- cbind(y_train, subject_train, x_train)
+mrg_test <- cbind(y_test, subject_test, x_test)
+
+setAllInOne <- rbind(mrg_train, mrg_test)
+
+# Extracting only the measurements on the mean and standard deviation for each measurement
+colNames <- colnames(setAllInOne)
+
+mean_and_std <- (grepl("activityId" , colNames) |
+                   grepl("subjectId" , colNames) | 
+                   grepl("mean" , colNames) | 
+                   grepl("std" , colNames) 
+)
+setForMeanAndStd <- setAllInOne[ , mean_and_std == TRUE]
+
+# Using descriptive activity names to name the activities in the data set
+setWithActivityNames <- merge(setForMeanAndStd, activityLabels,
+                              by='activityId',
+                              all.x=TRUE)
+
+# Creating a second, independent tidy data set with the average of each variable for each activity and each subject:
+secTidySet <- aggregate(. ~subjectId + activityId, setWithActivityNames, mean)
+secTidySet2 <- secTidySet[order(secTidySet$subjectId, secTidySet$activityId),]
+
+
+write.table(secTidySet2, "secTidySet.txt", row.name=FALSE)
